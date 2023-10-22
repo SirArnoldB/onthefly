aaf5fa8f60e9737f2f0f1ca53e11503b60f18ffb
Arnold Bhebhe <legendaryarnoldb@gmail.com>
2023-10-22 00:35:22 -0500

    add controllers + routes

--- server/server.js
+++ server/server.js
@@ -1,26 +1,26 @@
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
+import tripRoutes from './routes/trips.js'
+import activityRoutes from './routes/activities.js'
+import destinationRoutes from './routes/destinations.js'
+import tripDestinationRoutes from './routes/trips_destinations.js'
 
-// create express app
-const app = express();
+const app = express()
 
-// parse requests of content-type - application/json
-app.use(express.json());
+app.use(express.json())
+app.use(cors())
 
-// enable cors
-app.use(cors());
-
-// default route hander for / route (GET) request 
 app.get('/', (req, res) => {
-    res.status(200).send(
-        '<h1 style = "text-align: center; margin-top: 100px; font-size: 50px; color: #1a1a1a; font-weight: 1000;" >✈️ On The Fly API</h1>')
-});
+    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">✈️ On the Fly API</h1>')
+})
 
-// define the port to run the server on
-const PORT = process.env.PORT || 3001;
+app.use('/api/trips', tripRoutes)
+app.use('/api/activities', activityRoutes)
+app.use('/api/destinations', destinationRoutes)
+app.use('/api/trips-destinations', tripDestinationRoutes)
 
-// listen for requests
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
-    console.log(`🚀 Server is running on port ${PORT}.`);
-});
-
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+}) 
